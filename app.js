436382f92ccabd5dbd27f4f7d936d18632dce46e
‘Shelby <sls1881@gmail.com>
2021-01-11 17:40:42 -0800

    HTML, JS, and Tests

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
-// import functions and grab DOM elements
+// import functions 
+import { yesAnswer } from './utils.js';
+
+//Grab DOM elements
+const quizButton = document.getElementById('quiz-button');
+const quizResult = document.getElementById('quiz-result');
 
 // initialize state
 
+
 // set event listeners to update state and DOM
+quizButton.addEventListener('click', () => {
+
+    //Alert    
+    alert("It's time to take the quiz");
+
+    const confirmation = confirm('Do you want to proceed?');
+
+    if (!confirmation) return;
+
+    let correctAnswerCount = 0;
+
+    //Name prompts
+    const firstName = prompt("What's your first name?");
+    const lastName = prompt("What's your last name?");
+
+
+    //Dog question prompts
+    const dogTypeAnswer = prompt('Is Chloe a Chiweenie?');
+
+    if (yesAnswer(dogTypeAnswer)) correctAnswerCount++;
+
+    const dogWellAnswer = prompt('Does Chloe dog very well?');
+
+    if (!yesAnswer(dogWellAnswer)) correctAnswerCount++;
+
+    const dogFiveAnswer = prompt('Can Chloe high-five?');
+
+    if (yesAnswer(dogFiveAnswer)) correctAnswerCount++;
+
+    //Results
+    const resultString = `Hello ${firstName} ${lastName}! You got ${Math.round(correctAnswerCount / 3 * 100)}% questions right.`;
+
+    quizResult.textContent = resultString;
+
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,28 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { yesAnswer } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('It should increment by one when a word that starts with a Y is provided', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
-    
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = yesAnswer('Yo');
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('It should increment by one when a word that does not start with Y is provided', (expect) => {
+
+    const expected = false;
+
+    const actual = yesAnswer('No');
+
+    expect.equal(actual, expected);
+
+});
+
+
